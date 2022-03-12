52df6affed5ec041f81e21ec374599e17d3f4ec1
DisaroSkell <Hydralisk34980@gmail.com>
2022-03-12 21:34:52 +0100

    Modified server/main.js for tests

--- server/main.js
+++ server/main.js
@@ -1,4 +1,13 @@
-express = require('express')
-app = express()
-port = 6942
+const express = require('express')
+const app = express()
+const port = 6942
 
+var test = ['test1', 'test2', 'test3']
+
+app.get('/', function (req,res) {
+    res.status(200).json(test)
+})
+
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`)
+})
