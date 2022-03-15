34afcdd8d156d714695a02e885aad7a0ff8ed69c
DisaroSkell <Hydralisk34980@gmail.com>
2022-03-15 22:11:47 +0100

    read, readall, update and delete from enemies.js

--- server/models/Enemies.js
+++ server/models/Enemies.js
@@ -2,8 +2,8 @@ const client = require("../db")
 
 function createEnemy(name, health_max) {
     return new Promise((resolve, reject) => {
-        var values = [name, health_max]
-        let sql = `insert into enemies (namee,healthe_max) values ($1, $2);`
+        const values = [name, health_max]
+        const sql = `insert into enemies (namee,healthe_max) values ($1, $2);`
         client.query(sql, values, (err,result) => {
             if (err) {
                 return console.error(err.message)
@@ -12,4 +12,68 @@ function createEnemy(name, health_max) {
             resolve(result)
         })
     })
+}
+
+function readEnemy(id) {
+    return new Promise((resolve, reject) => {
+        const values = [id]
+        const sql = `select * from enemies where iden = $1;`
+        client.query(sql, values, (err,result) => {
+            if (err) {
+                return console.error(err.message)
+            }
+
+            resolve(result)
+        })
+    })
+}
+
+function readAllEnemies() {
+    return new Promise((resolve, reject) => {
+        const values = []
+        const sql = `select * from enemies;`
+        client.query(sql, values, (err,result) => {
+            if (err) {
+                return console.error(err.message)
+            }
+
+            resolve(result)
+        })
+    })
+}
+
+function updateEnemy(id, name, health_max) {
+    return new Promise((resolve, reject) => {
+        const values = [id, name, health_max]
+        const sql = `update table set namee = $2, healthe_max = $3 where iden = $1;`
+        client.query(sql, values, (err,result) => {
+            if (err) {
+                return console.error(err.message)
+            }
+
+            resolve(result)
+        })
+    })
+}
+
+function deleteEnemy(id) {
+    return new Promise((resolve, reject) => {
+        const values = [id]
+        const sql = `delete from enemies where iden = $1;`
+        client.query(sql, values, (err,result) => {
+            if (err) {
+                return console.error(err.message)
+            }
+
+            resolve(result)
+        })
+    })
+}
+
+module.exports = {
+    createEnemy,
+    readEnemy,
+    readAllEnemies,
+    updateEnemy,
+    deleteEnemy
 }
