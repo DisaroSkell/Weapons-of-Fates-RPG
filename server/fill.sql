078647f6a3871fe6cc38b6f1e2844ace4ddf7ebf
DisaroSkell <Hydralisk34980@gmail.com>
2022-03-15 17:01:47 +0100

    Modified fill to match the serial type of id

--- server/fill.sql
+++ server/fill.sql
@@ -1,17 +1,17 @@
-insert into Ennemies values ('Goblin', 50);
+insert into Ennemies (NameE, HealthE_Max) values ('Goblin', 50);
 
-insert into TypeW values ('Sword');
+insert into TypeW (NameTW) values ('Sword');
 
-insert into Fate values ('Dust');
+insert into Fate (NameF) values ('Dust');
 
-insert into Weapons values ('Rusty Sword', 10, 0, 100, 0, 0);
+insert into Weapons (NameW, DamageW, Xp, Xp_Max, Fate_Level, Weapon_Type) values ('Rusty Sword', 10, 0, 100, 1, 1);
 
-insert into Outfits values ('Basic', './img/basic_outfit');
+insert into Outfits (NameO, Sprite) values ('Basic', './img/basic_outfit');
 
-insert into Player values ('Hector', 'hect.tp@gmail.com', '1234', 70, 50, 0, 0);
+insert into Player values ('Hector', 'hect.tp@gmail.com', '1234', 70, 50, 1, 1);
 
-insert into Abilities values ('Fire', 30, 50);
+insert into Abilities (NameA, DamageA, Mana_Cost) values ('Fire', 30, 50);
 
-insert into Can_Use values (0, 0);
+insert into Can_Use values (1, 1);
 
-insert into Weapon_Skills values (0, 0);
+insert into Weapon_Skills values (1, 1);
