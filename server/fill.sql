693521759b7d757a553c731ba118b0b76b88aa3f
DisaroSkell <Hydralisk34980@gmail.com>
2022-03-15 12:40:54 +0100

    Added a file.sql file

---
+++ server/fill.sql
@@ -0,0 +1,17 @@
+insert into Ennemies values (0, 'Goblin', 50);
+
+insert into TypeW values (0, 'Sword');
+
+insert into Fate values (0, 'Dust');
+
+insert into Weapons values (0, 'Rusty Sword', 10, 0, 100, 0, 0);
+
+insert into Outfits values (0, 'Basic', './img/basic_outfit');
+
+insert into Player values ('Hector', 'hect.tp@gmail.com', '1234', 70, 50, 0, 0);
+
+insert into Abilities values (0, 'Fire', 30, 50);
+
+insert into Can_Use values (0, 0);
+
+insert into Weapon_Skills values (0, 0);
