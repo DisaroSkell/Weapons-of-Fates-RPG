58a0b35e82100866fd80ed92d9ae1df5fc7c9e61
DisaroSkell <Hydralisk34980@gmail.com>
2022-03-15 18:18:34 +0100

    Added tables creation and fill in db.js

--- server/db.js
+++ server/db.js
@@ -1,3 +1,4 @@
+const fs = require('fs')
 const { Client } = require('pg')
 
 const client = new Client({
@@ -12,3 +13,33 @@ client.connect(function(err) {
   if (err) throw err;
   console.log("Connected to database " + client.database + "!");
 })
+
+const sql_create = fs.readFile(__dirname + "/tables.sql", function (err, data) {
+    if (err) {
+        return console.error(err.message);
+    }
+    else {
+        client.query(data.toString(), [], (err, result) => {
+            if (err) {
+                return console.error(err.message);
+            }
+            console.log("Tables successfully created");
+        });
+    }
+});
+
+const sql_fill = fs.readFile(__dirname + "/fill.sql", function (err, data) {
+    if (err) {
+        return console.error(err.message);
+    }
+    else {
+        client.query(data.toString(), [], (err, result) => {
+            if (err) {
+                return console.error(err.message);
+            }
+            console.log("Tables successfully filled");
+        });
+    }
+});
+
+module.exports = client
