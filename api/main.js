b9bde42faa184c3b0657cd3bee49d1ad4ebf2ded
DisaroSkell <Hydralisk34980@gmail.com>
2022-03-17 18:12:07 +0100

    Added CORS to api

--- api/main.js
+++ api/main.js
@@ -1,7 +1,12 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const cors = require("cors")
 const app = express()
 const port = 6942
+
+const corsOptions = {
+    origin: "*"
+}
 
 // parse requests of content-type - application/json
 app.use(bodyParser.json())
