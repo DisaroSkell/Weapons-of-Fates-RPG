321c907fc0ce7a3326d7e024fa9f1635306bd839
DisaroSkell <Hydralisk34980@gmail.com>
2022-03-18 19:20:13 +0100

    Modified updatePlayer function

--- api/models/Player.js
+++ api/models/Player.js
@@ -42,10 +42,11 @@ function readAllPlayers() {
     })
 }
 
-function updatePlayer(username, email, password, health_max, mana_max) {
+function updatePlayer(username, email, password, health_max, current_health, mana_max, current_mana, gold, weapon, outfit, ability1, ability2) {
     return new Promise((resolve, reject) => {
-        const values = [username, email, password, health_max, mana_max]
-        const sql = `update player set email = $2, password = $3, health_maxp = $4, mana_max = $5 where username = $1;`
+        const values = [username, email, password, health_max, current_health, mana_max, current_mana, gold, weapon, outfit, ability1, ability2]
+        const sql = `update player set email = $2, password = $3, health_maxp = $4, current_health = $5, mana_max = $6,
+        current_mana = $7, gold_purse = $8, chosen_weapon = $9, chosen_outfit = $10, chosen_ability1 = $11, chosen_ability2 = $12 where username = $1;`
         db.query(sql, values, (err,result) => {
             if (err) {
                 return console.error(err.message)
