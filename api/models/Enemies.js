bac199123771a9e755c345aa8efd6ade7adc3dea
DisaroSkell <Hydralisk34980@gmail.com>
2022-03-18 11:19:58 +0100

    Updated enemies CRUD

--- api/models/Enemies.js
+++ api/models/Enemies.js
@@ -1,9 +1,9 @@
 const client = require("../db")
 
-function createEnemy(name, health_max) {
+function createEnemy(name, health_max, xp, gold, strength, resistance, weekness) {
     return new Promise((resolve, reject) => {
-        const values = [name, health_max]
-        const sql = `insert into enemies (namee,health_maxe) values ($1, $2);`
+        const values = [name, health_max, xp, gold, strength, resistance, weekness]
+        const sql = `insert into enemies (namee, health_maxe, xp_reward, gold_reward, strength, resistance, weekness) values ($1, $2, $3, $4, $5, $6 ,$7);`
         client.query(sql, values, (err,result) => {
             if (err) {
                 return console.error(err.message)
@@ -42,10 +42,10 @@ function readAllEnemies() {
     })
 }
 
-function updateEnemy(id, name, health_max) {
+function updateEnemy(id, name, health_max, xp, gold, strength, resistance, weekness) {
     return new Promise((resolve, reject) => {
-        const values = [id, name, health_max]
-        const sql = `update table set namee = $2, health_maxe = $3 where ide = $1;`
+        const values = [id, name, health_max, xp, gold, strength, resistance, weekness]
+        const sql = `update table set namee = $2, health_maxe = $3, xp_reward = $4, gold_reward = $5, strength = $6, resistance = $7, weekness = $8 where ide = $1;`
         client.query(sql, values, (err,result) => {
             if (err) {
                 return console.error(err.message)
@@ -56,7 +56,7 @@ function updateEnemy(id, name, health_max) {
     })
 }
 
-function deleteEnemy(id) {
+function removeEnemy(id) {
     return new Promise((resolve, reject) => {
         const values = [id]
         const sql = `delete from enemies where ide = $1;`
@@ -75,5 +75,5 @@ module.exports = {
     readEnemy,
     readAllEnemies,
     updateEnemy,
-    deleteEnemy
+    removeEnemy
 }
--- api/routes/Enemies.js
+++ api/routes/Enemies.js
@@ -2,9 +2,15 @@ module.exports = app => {
     const router = require('express').Router()
     const controller = require('../controllers/Enemies')
 
-    router.post('/', controller.addEnemy)
+    router.post('/', controller.postEnemy)
+    
+    router.get('/:id', controller.getEnemy)
     
     router.get('/', controller.getAllEnemies)
+    
+    router.put('/:id', controller.putEnemy)
+    
+    router.delete('/:id', controller.deleteEnemy)
     
     app.use('/enemies', router)
 }
