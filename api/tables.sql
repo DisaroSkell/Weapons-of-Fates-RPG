eed29825cd982c27bf684f1814527f11b2d2d332
DisaroSkell <Hydralisk34980@gmail.com>
2022-03-21 10:14:11 +0100

    Constraints in tables now

--- api/tables.sql
+++ api/tables.sql
@@ -3,7 +3,10 @@
 create table if not exists Rarity (
     IDR serial unique,
     NameR varchar(20),
-    primary key (IDR)
+
+    primary key (IDR),
+
+    constraint nonemptynamer check (char_length(isnull(NameR, '')) > 0)
 );
 
 --Weapon Types table
@@ -11,7 +14,10 @@ create table if not exists Rarity (
 create table if not exists TypeW (
     IDT serial unique,
     NameTW varchar(20),
-    primary key (IDT)
+    
+    primary key (IDT),
+
+    constraint nonemptynametw check (char_length(isnull(NameTW, '')) > 0)
 );
 
 --Enemies table
@@ -25,10 +31,16 @@ create table if not exists Enemies (
     Strength integer,
     Resistance integer,
     Weekness integer,
+
+    primary key (IDE),
+
     constraint FK_Enemies1 foreign key (Strength) references Rarity(IDR),
     constraint FK_Enemies2 foreign key (Resistance) references TypeW(IDT),
     constraint FK_Enemies3 foreign key (Weekness) references TypeW(IDT),
-    primary key (IDE)
+
+    constraint nonemptynamee check (char_length(isnull(NameE, '')) > 0),
+    constraint notnullhealthe check (isnull(Health_MaxE, 0) > 0),
+    constraint notnullstrength check (Strength is not null)
 );
 
 --Fate table
@@ -37,7 +49,11 @@ create table if not exists Fate (
     IDF serial unique,
     NameF varchar(20),
     DescF varchar(500),
-    primary key (IDF)
+
+    primary key (IDF),
+
+    constraint nonemptynamef check (char_length(isnull(NameF, '')) > 0),
+    constraint nonemptydesc check (char_length(isnull(DescF, '')) > 0)
 );
 
 --Weapons table
@@ -49,9 +65,17 @@ create table if not exists Weapons (
     Xp_Max integer,
     Weapon_Type integer,
     Rarity integer,
+
     primary key (IDW),
+
     constraint FK_Weapons1 foreign key (Weapon_Type) references TypeW(IDT),
-    constraint FK_Weapons2 foreign key (Rarity) references Rarity(IDR)
+    constraint FK_Weapons2 foreign key (Rarity) references Rarity(IDR),
+
+    constraint nonemptynamew check (char_length(isnull(NameW, '')) > 0),
+    constraint notnulldamagew check (isnull(DamageW, 0) > 0),
+    constraint notnullxpw check (isnull(Xp_Max, 0) > 0),
+    constraint notnulltypew check (Weapon_Type is not null),
+    constraint notnullrarityw check (Rarity is not null)
 );
 
 --Outfits table (if enough time)
@@ -61,7 +85,12 @@ create table if not exists Outfits (
     NameO varchar(20),
     Sprite varchar(50),
     Price integer,
-    primary key (IDO)
+
+    primary key (IDO),
+
+    constraint nonemptynameo check (char_length(isnull(NameO, '')) > 0),
+    constraint nonemptysprite check (isnull(Sprite, 0) > 0),
+    constraint notnullprice check (isnull(Price, 0) > 0)
 );
 
 --Abilities table
@@ -71,7 +100,12 @@ create table if not exists Abilities (
     NameA varchar(20),
     DamageA integer,
     Mana_Cost integer,
-    primary key (IDA)
+
+    primary key (IDA),
+
+    constraint nonemptynamea check (char_length(isnull(NameA, '')) > 0),
+    constraint notnulldamagea check (isnull(DamageA, 0) > 0),
+    constraint notnullmanacost check (isnull(Mana_Cost, 0) > 0)
 );
 
 --Player table
@@ -89,11 +123,23 @@ create table if not exists Player (
     Chosen_Outfit integer,
     Chosen_Ability1 integer,
     Chosen_Ability2 integer,
+
     primary key (Username),
+
     constraint FK_Player1 foreign key (Chosen_Weapon) references Weapons(IDW),
     constraint FK_Player2 foreign key (Chosen_Outfit) references Outfits(IDO),
     constraint FK_Player3 foreign key (Chosen_Ability1) references Abilities(IDA),
-    constraint FK_Player4 foreign key (Chosen_Ability2) references Abilities(IDA)
+    constraint FK_Player4 foreign key (Chosen_Ability2) references Abilities(IDA),
+
+    constraint nonemptynamep check (char_length(isnull(Username, '')) > 0),
+    constraint nonemptyemail check (char_length(isnull(Email, '')) > 0),
+    constraint nonemptypassword check (char_length(isnull(Password, '')) > 0),
+    constraint notnullhealthp check (isnull(Health_MaxP, 0) > 0),
+    constraint notnullmanap check (isnull(Mana_Max, 0) > 0),
+    constraint notnullchosenweapon check (Chosen_Weapon is not null),
+    constraint notnullchosenoutfit check (Chosen_Outfit is not null),
+    constraint notnullchosenability1 check (Chosen_Ability1 is not null),
+    constraint notnullchosenability2 check (Chosen_Ability2 is not null)
 );
 
 --Can_Use table (link between Enemies and Abilities tables)
@@ -102,7 +148,10 @@ create table if not exists Can_Use (
     IDA integer,
     IDE integer,
     Use_Prio integer,
-    primary key (IDA,IDE)
+
+    primary key (IDA,IDE),
+
+    constraint notnulluseprio check (isnull(Use_Prio, 0) > 0)
 );
 
 --Weapons_Inventory table (link between Player and Weapons tables)
@@ -113,8 +162,12 @@ create table if not exists Weapons_Inventory (
     Xp integer,
     FOOTs_Won integer,
     Fate_Level integer,
+
     primary key (IDW,IDP),
-    constraint FK_Weapons_Inventory foreign key (Fate_Level) references Fate(IDF)
+    
+    constraint FK_Weapons_Inventory foreign key (Fate_Level) references Fate(IDF),
+
+    constraint notnullfatelevel check (isnull(Fate_Level, 0) > 0)
 );
 
 --Outfits_Inventory table (link between Player and Outfit tables)
@@ -122,6 +175,7 @@ create table if not exists Weapons_Inventory (
 create table if not exists Outfits_Inventory (
     IDO integer,
     IDP integer,
+
     primary key (IDO,IDP)
 );
 
@@ -130,6 +184,7 @@ create table if not exists Outfits_Inventory (
 create table if not exists Abilities_Inventory (
     IDA integer,
     IDP integer,
+    
     primary key (IDA,IDP)
 );
 
@@ -138,44 +193,3 @@ create table if not exists Abilities_Inventory (
 /*create table if not exists FOOT (
     IDFOOT integer
 );*/
-
---Constraints for all tables
-
-alter table Rarity add constraint if not exists nonemptynamer check (char_length(isnull(NameR, '')) > 0);
-
-alter table TypeW add constraint if not exists nonemptynametw check (char_length(isnull(NameTW, '')) > 0);
-
-alter table Enemies add constraint if not exists nonemptynamee check (char_length(isnull(NameE, '')) > 0);
-alter table Enemies add constraint if not exists notnullhealthe check (isnull(Health_MaxE, 0) > 0);
-alter table Enemies add constraint if not exists notnullstrength check (Strength is not null);
-
-alter table Fate add constraint if not exists nonemptynamef check (char_length(isnull(NameF, '')) > 0);
-alter table Fate add constraint if not exists nonemptydesc check (char_length(isnull(DescF, '')) > 0);
-
-alter table Weapons add constraint if not exists nonemptynamew check (char_length(isnull(NameW, '')) > 0);
-alter table Weapons add constraint if not exists notnulldamagew check (isnull(DamageW, 0) > 0);
-alter table Weapons add constraint if not exists notnullxpw check (isnull(Xp_Max, 0) > 0);
-alter table Weapons add constraint if not exists notnulltypew check (Weapon_Type is not null);
-alter table Weapons add constraint if not exists notnullrarityw check (Rarity is not null);
-
-alter table Outfits add constraint if not exists nonemptynameo check (char_length(isnull(NameO, '')) > 0);
-alter table Outfits add constraint if not exists nonemptysprite check (isnull(Sprite, 0) > 0);
-alter table Outfits add constraint if not exists notnullprice check (isnull(Price, 0) > 0);
-
-alter table Abilities add constraint if not exists nonemptynamea check (char_length(isnull(NameA, '')) > 0);
-alter table Abilities add constraint if not exists notnulldamagea check (isnull(DamageA, 0) > 0);
-alter table Abilities add constraint if not exists notnullmanacost check (isnull(Mana_Cost, 0) > 0);
-
-alter table Player add constraint if not exists nonemptynamep check (char_length(isnull(Username, '')) > 0);
-alter table Player add constraint if not exists nonemptyemail check (char_length(isnull(Email, '')) > 0);
-alter table Player add constraint if not exists nonemptypassword check (char_length(isnull(Password, '')) > 0);
-alter table Player add constraint if not exists notnullhealthp check (isnull(Health_MaxP, 0) > 0);
-alter table Player add constraint if not exists notnullmanap check (isnull(Mana_Max, 0) > 0);
-alter table Player add constraint if not exists notnullchosenweapon check (Chosen_Weapon is not null);
-alter table Player add constraint if not exists notnullchosenoutfit check (Chosen_Outfit is not null);
-alter table Player add constraint if not exists notnullchosenability1 check (Chosen_Ability1 is not null);
-alter table Player add constraint if not exists notnullchosenability2 check (Chosen_Ability2 is not null);
-
-alter table Can_Use add constraint if not exists notnulluseprio check (isnull(Use_Prio, 0) > 0);
-
-alter table Weapons_Inventory add constraint if not exists notnullfatelevel check (isnull(Fate_Level, 0) > 0);
