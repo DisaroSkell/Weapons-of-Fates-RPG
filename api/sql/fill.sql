56f3f2031d317fdee09a16b375731854bcb03e12
DisaroSkell <Hydralisk34980@gmail.com>
2022-04-01 16:08:52 +0200

    Added things in fill.sql

--- api/sql/fill.sql
+++ api/sql/fill.sql
@@ -1,4 +1,7 @@
 insert into Rarity (NameR) values ('Common');
+insert into Rarity (NameR) values ('Rare');
+insert into Rarity (NameR) values ('Epic');
+insert into Rarity (NameR) values ('Legendary');
 
 insert into TypeW (NameTW) values ('Sword');
 
